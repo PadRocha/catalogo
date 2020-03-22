a918741585833a60ace1cd288d4632b485e75550
Joshua Elí Padrón Rocha <p.roch@outlook.es>
2020-03-21 22:40:43 -0600

    delete jquery

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -9,7 +9,6 @@ import { Key } from 'src/app/models/key';
 import { Line } from 'src/app/models/line';
 import { Image } from 'src/app/models/image';
 
-import * as $ from 'jquery';
 declare const alertify: any;
 
 @Component({
